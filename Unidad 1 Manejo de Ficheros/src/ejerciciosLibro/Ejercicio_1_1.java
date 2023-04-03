fd1cd70f41f02c535661ba1cf192c943fdd5224f
borjagv1 <borjagv1@gmail.com>
2023-04-03 19:32:55 +0200

    probando repo

--- Unidad 1 Manejo de Ficheros/src/ejerciciosLibro/Ejercicio_1_1.java
+++ Unidad 1 Manejo de Ficheros/src/ejerciciosLibro/Ejercicio_1_1.java
@@ -1,4 +1,5 @@
-public class App {
+package ejerciciosLibro;
+public class Ejercicio_1_1 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
