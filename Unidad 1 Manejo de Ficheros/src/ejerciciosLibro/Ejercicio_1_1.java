35790bcf2fc99f55802506c91ce1eaa6cf994f47
borjagv1 <borjagv1@gmail.com>
2023-04-03 20:17:07 +0200

    Actividad 1.1 completada

--- Unidad 1 Manejo de Ficheros/src/ejerciciosLibro/Ejercicio_1_1.java
+++ Unidad 1 Manejo de Ficheros/src/ejerciciosLibro/Ejercicio_1_1.java
@@ -1,6 +1,20 @@
 package ejerciciosLibro;
+
+import java.io.File;
+
 public class Ejercicio_1_1 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String dir = "C:\\xampp"; // directorio actual o args[0];
+        File f = new File(dir);
+        String[] archivos = f.list();
+        if (archivos == null) {
+            System.out.println(dir + " No es un directorio válido");
+        } else {
+            System.out.println("Archivos en el directorio actual: " + dir + " " + archivos.length);
+            for (int i = 0; i < archivos.length; i++) {
+                System.out.println("Nombre: " + archivos[i]); 
+            } 
+        }
+
     }
 }
