0896a32f8aba3fea0852d8e70b49f6a97cdcd140
borjagv1 <borjagv1@gmail.com>
2023-06-06 19:05:12 +0200

    Examen 2022 Completado

--- Unidad 5 Bases de datos NoSQL/RA5_BBDD_NoSQL/src/main/java/examen2022/Main_Examen2022.java
+++ Unidad 5 Bases de datos NoSQL/RA5_BBDD_NoSQL/src/main/java/examen2022/Main_Examen2022.java
@@ -30,18 +30,115 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Updates.*;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import static com.mongodb.client.model.Sorts.*;
 
 public class Main_Examen2022 {
+
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         MongoClient cliente = MongoClients.create();
         MongoDatabase db = cliente.getDatabase("GuerraValenciaBorja");
         MongoCollection<Document> coleccionZonas = db.getCollection("zonas");
         MongoCollection<Document> coleccionVendedores = db.getCollection("vendedores");
         MongoCollection<Document> coleccionVentas = db.getCollection("ventas");
-        
-        ActualizarZonas(coleccionZonas, coleccionVendedores, coleccionVentas);
-        ActualizarVendedores(coleccionVendedores, coleccionVentas, coleccionZonas);
+
+         ActualizarZonas(coleccionZonas, coleccionVendedores, coleccionVentas);
+         ActualizarVendedores(coleccionVendedores, coleccionVentas, coleccionZonas);
+         ListarZonas(coleccionZonas, coleccionVendedores, coleccionVentas);
+
+    }
+
+    private static void ListarZonas(MongoCollection<Document> coleccionZonas,
+            MongoCollection<Document> coleccionVendedores, MongoCollection<Document> coleccionVentas) {
+
+        // Proceso repetitivo en el que se pide por teclado el ID de una zona y se
+        // muestran los vendedores de la zona y las ventas realizadas en la zona. Si la
+        // zona no existe en la colección de zonas se mostrará un mensaje indicándolo.
+        // El proceso termina cuando se introduce un 0 como ID de zona.
+
+        System.out.println("===================================================================");
+        int id = leerEntero("Introduce el ID de una zona: ", 0, 9999);
+        System.out.println("===================================================================");
+        while (id != 0) {
+            Document zona = coleccionZonas.find(eq("_id", id)).first();
+            if (zona == null) {
+                System.out.println("NO EXISTE EL ID DE LA ZONA");
+            } else {
+                // MUestro datos de la zona: id, nombrezona, provincia, ventas, importeTotal
+
+                // formateo el importetotal para que salga con , para decimales y . para los
+                // miles
+                Double importeTotal = ((Number) zona.get("importeTotal")).doubleValue();
+                String importeTotalFormateado = String.format("%,.2f", importeTotal);
+                System.out.println("===================================================================");
+
+                System.out.println("Zona: " + zona.get("_id") + ", " + zona.get("nombrezona") + ", "
+                        + zona.get("provincia") + "\n Numero de Ventas: " + zona.get("ventas") + " Importe total: "
+                        + importeTotalFormateado);
+                System.out.println("===================================================================");
+                // Mostrar vendedores de la zona SI NO TIENE VENDEDORES MUESTRO UN MENSAJE:
+                // id NOMBRE IMPORTETOTAL NVENTAS
+                // Compruebo que haya vendedores en la zona
+
+                boolean hayVendedores = coleccionVendedores.find(eq("codZona", id)).first() != null;
+
+                if (!hayVendedores) {
+                    System.out.println("\t<< NO HAY VENDEDORES EN LA ZONA>> ");
+
+                } else {
+                    System.out.println("LISTADO DE VENDEDORES EN LA ZONA: " + zona.get("nombrezona"));
+                    // cabecera: ID NOMBRE IMPORTE TOTAL Nº VENTAS ==== ===============
+                    // ============= =========
+                    System.out.printf("\t%-10s %-15s %-15s %-15s\n", "ID", "NOMBRE", "IMPORTE TOTAL", "Nº VENTAS");
+                    System.out.printf("\t%-10s %-15s %-15s %-15s\n", "===", "==============", "==============",
+                            "==============");
+                    for (Document vendedor : coleccionVendedores.find(eq("codZona", id))) {
+                        Double importeTotalaFormatear = (Double) vendedor.get("importeTotal");
+                        String importeTotalFormateado2 = String.format("%,.2f", importeTotalaFormatear);
+
+                        System.out.printf("\t%-10s %-20s %-22s %-15s\n", vendedor.get("_id"), vendedor.get("nombre"),
+                                importeTotalFormateado2, vendedor.get("nventas"));
+
+                    }
+                }
+                System.out.println();
+                // AHORA MUESTRO LAS VENTAS DE LA ZONA: ID, NOMBREVENDEDOR IMPORTEVENTA
+                // Compruebo que haya ventas en la zona
+                boolean hayVentas = coleccionVentas.find(eq("zona", id)).first() != null;
+                if (!hayVentas) {
+                    System.out.println("\t<< NO HAY VENTAS EN LA ZONA>> ");
+                } else {
+                    System.out.println("LISTADO DE VENTAS EN LA ZONA: " + zona.get("nombrezona"));
+                    for (Document venta : coleccionVentas.find(eq("zona", id))) {
+                        Document vendedor = coleccionVendedores.find(eq("_id", venta.get("vendedor"))).first();
+                        // compruebo que el vendedor exista si no, escribo, no existe
+                        if (vendedor == null) {
+                            // Cabecera ID NOMBRE VENDEDOR IMPORTE VENTA ==== =============== =============
+                            System.out.printf("\t%-10s %-15s %-15s\n", "ID", "NOMBRE VENDEDOR", "IMPORTE VENTA");
+                            System.out.printf("\t%-10s %-15s %-15s\n", "===", "==============", "==============");
+                            System.out.printf("\t%-10s %-15s %-15s\n", venta.get("vendedor"), "no existe",
+                                    venta.get("importeventa"));
+                        } else {
+                            System.out.printf("\t%-10s %-15s %-15s\n", "ID", "NOMBRE VENDEDOR", "IMPORTE VENTA");
+                            System.out.printf("\t%-10s %-15s %-15s\n", "===", "==============", "==============");
+                            System.out.printf("\t%-10s %-15s %-15s\n", venta.get("vendedor"), vendedor.get("nombre"),
+                                    venta.get("importeventa"));
+
+                        }
+
+                    }
+                    System.out.println("===================================================================");
+                }
+            }
+            System.out.println("===================================================================");
+            id = leerEntero("Introduce el ID de una zona: ", 0, 9999);
+            System.out.println("===================================================================");
+        }
     }
 
     private static void ActualizarVendedores(MongoCollection<Document> coleccionVendedores,
@@ -58,7 +155,9 @@ public class Main_Examen2022 {
 
         // Inicializo a *No existe* el campo nombreZona de la colección vendedores
         coleccionVendedores.updateMany(exists("nombreZona", true), set("nombreZona", "*No existe*"));
-
+        // Pongo 0 en el campo nventas e importeTotal de la colección vendedores
+        coleccionVendedores.updateMany(exists("importeTotal", true), set("importeTotal", 0));
+        coleccionVendedores.updateMany(exists("nventas", true), set("nventas", 0)); 
         for (Document ventas : coleccionVentas.find().sort(ascending("vendedor"))) {
             int vendedor = ventas.getInteger("vendedor");
             int zona = ventas.getInteger("zona");
@@ -91,17 +190,17 @@ public class Main_Examen2022 {
             // si no hay errores actualizo los campos
             if (err == 0) {
                 String zonaNombre = coleccionZonas.find(eq("_id", zona)).first().getString("nombrezona");
-                
+
                 coleccionVendedores.updateOne(eq("_id", vendedor), set("nombreZona", zonaNombre));
-                
+
                 // El campo nventas para que almacene el nº de ventas que ha realizado el
                 // vendedor. Cada documento en la colección de ventas es una venta.
-                
+
                 coleccionVendedores.updateOne(eq("_id", vendedor), inc("nventas", 1));
 
                 // El campo importeTotal, a este campo se deben sumar los importeventa del
                 // vendedor que se encuentran en la colección de ventas
-                
+
                 coleccionVendedores.updateOne(eq("_id", vendedor), inc("importeTotal", importeVenta));
             }
             err = 0;
@@ -113,8 +212,8 @@ public class Main_Examen2022 {
     private static void ActualizarZonas(MongoCollection<Document> coleccionZonas,
             MongoCollection<Document> coleccionVendedores,
             MongoCollection<Document> coleccionVentas) {
-     
-                int err = 0;
+
+        int err = 0;
 
         // Actualizar en la colección de zonas:
         // El campo ventas para que contenga el nº de ventas que se han realizado en la
@@ -159,4 +258,32 @@ public class Main_Examen2022 {
         System.out.println("Fin de la actualización de zonas");
         System.out.println("=====================================================================================");
     }
+
+    private static int leerEntero(String mensaje, int min, int max) {
+        boolean salir = false;
+        int numero = 0;
+
+        do {
+            try {
+                System.out.print(mensaje);
+                numero = sc.nextInt();
+                sc.nextLine();
+                while (numero < min || numero > max) {
+                    System.out.print("\tSuperado l mite (> " + min + " y < " + max + ")");
+                    System.out.print("\n\tOtra vez: ");
+
+                    numero = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println();
+                }
+                salir = true;
+
+            } catch (InputMismatchException exc) {
+                sc.nextLine();
+                System.out.print("\n\tIncorrecto, escríbelo de nuevo: ");
+            }
+        } while (!salir);
+
+        return numero;
+    }// LecturaEntero
 }
